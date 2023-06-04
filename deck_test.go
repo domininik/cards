087a9e3c151801891c077d5ddda41d5f737a6b1d
domininik <dominik.fijas+github@gmail.com>
2023-06-04 22:29:46 +0200

    Add some more TestNewDeck tests

--- deck_test.go
+++ deck_test.go
@@ -10,4 +10,10 @@ func TestNewDeck(t *testing.T) {
 	deck := newDeck()
 
 	assert.Equal(t, 52, len(deck), "Deck should have 52 cards")
+
+	assert.Equal(t, "Spades", deck[0].Suit, "First card should be Spades")
+	assert.Equal(t, "Ace", deck[0].Rank, "First card should be Ace")
+
+	assert.Equal(t, "Clubs", deck[51].Suit, "Last card should be Clubs")
+	assert.Equal(t, "King", deck[51].Rank, "Last card should be King")
 }
