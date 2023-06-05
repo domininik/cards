c6b55c284518cf36054b8c4615edb7ed32650339
domininik <dominik.fijas+github@gmail.com>
2023-06-05 09:48:38 +0200

    Add more Deck tests

--- deck_test.go
+++ deck_test.go
@@ -17,3 +17,23 @@ func TestNewDeck(t *testing.T) {
 	assert.Equal(t, "Clubs", deck[51].Suit, "Last card should be Clubs")
 	assert.Equal(t, "King", deck[51].Rank, "Last card should be King")
 }
+
+func TestDeal(t *testing.T) {
+	deck := newDeck()
+
+	hand := deck.deal(7)
+
+	assert.Equal(t, 7, len(hand), "Hand should have 7 cards")
+	assert.Equal(t, 45, len(deck), "Deck should have 45 cards")
+}
+
+func TestShuffle(t *testing.T) {
+	deck := newDeck()
+	anotherDeck := newDeck()
+
+	assert.Equal(t, deck, anotherDeck, "Decks should be equal")
+
+	deck.shuffle()
+
+	assert.NotEqual(t, deck, anotherDeck, "Decks should not be equal")
+}
